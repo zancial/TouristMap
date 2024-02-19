bd4d4916f07fbe4224c54b3d9748e7a84d93f17a
zancial <valeev.ilya2014@yandex.ru>
2024-02-20 00:27:23 +0500

    version 2.1

--- Desktop/TouristMap/script.js
+++ Desktop/TouristMap/script.js
@@ -1,27 +1,79 @@
+let position_person = [];
 let placemark_guk = [56.844058833520016, 60.65398059317325];
 
 function init() {
 	let map = new ymaps.Map("map-test", {
 		center: [56.844058833520016, 60.65398059317325],
-		zoom: 17
+		zoom: 17,
+		controls: ["routePanelControl"]
 	});
 
-	let placemark = new ymaps.Placemark(placemark_guk, {}, {
+	let control = map.controls.get('routePanelControl');
+
+	const options = {
+		enableHighAccuracy: true,
+		timeout: 5000,
+		maximumAge: 0
+	};
+
+	function success(pos) {
+		const crd = pos.coords;
+		let reverseGeocoder = ymaps.geocode([crd.latitude, crd.longitude]);
+		let locationText = null;
+		reverseGeocoder.then(function (res) {
+			locationText = res.geoObjects.get(0).properties.get('text')
+			console.log(locationText)
+
+			control.routePanel.state.set({
+				type: 'masstransit',
+				fromEnabled: false,
+				from: locationText,
+				toEnabled: true,
+				to: `Р•РєР°С‚РµСЂРёРЅР±СѓСЂРі, РњРёСЂР° 19`,
+			});
+		});
+
+		console.log(locationText)
+
+		control.routePanel.options.set({
+			types: {
+				masstransit: true,
+				pedestrian: true,
+				taxi: true,
+				auto: true
+			}
+		})
+	}
+
+	function error(err) {
+		console.warn(`ERROR(${err.code}): ${err.message}`);
+	}
+
+	navigator.geolocation.getCurrentPosition(success, error, options);
+
+	let placemark = new ymaps.Placemark(placemark_guk, {
+		balloonContentHeader: "РџСЂРёРІРµС‚",
+		balloonContentBody: [
+			"<img src = 'http://viu.tsu.ru/upload/resize_cache/iblock/1d8/300_1000_1/1d851be685e04b32be320012ba967b83.png' />",
+			"РџСЂРёРІРµС‚",
+		].join(""),
+		balloonContentFooter: "РџСЂРёРІРµС‚",
+		hintContent: "РЈСЂР°Р»СЊСЃРєРёР№ Р¤РµРґРµСЂР°Р»СЊРЅС‹Р№ РЈРЅРёРІРµСЂСЃРёС‚РµС‚",
+	}, {
 		iconLayout: "default#image",
 		iconImageHref: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
 		iconImageSize: [40, 40],
 		iconImageOffset: [-20, -35]
 	});
 
-	map.controls.remove('geolocationControl'); // удаляем геолокацию
-	map.controls.remove('searchControl'); // удаляем поиск
-	map.controls.remove('trafficControl'); // удаляем контроль трафика
-	map.controls.remove('typeSelector'); // удаляем тип
-	map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-	map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-	map.controls.remove('rulerControl'); // удаляем контрол правил
+	map.controls.remove('geolocationControl');
+	map.controls.remove('searchControl');
+	map.controls.remove('trafficControl');
+	map.controls.remove('typeSelector');
+	map.controls.remove('fullscreenControl');
+	map.controls.remove('zoomControl');
+	map.controls.remove('rulerControl');
 
 	map.geoObjects.add(placemark);
 }
 ymaps.ready(init);
-
